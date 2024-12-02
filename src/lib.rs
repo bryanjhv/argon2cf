6a89cde351c8b0f3cf02d1d8fef78bfc9d947e59
Bryan Horna <bryanjhv@gmail.com>
2024-12-02 17:35:12 -0500

    feat: first worker implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,37 @@
-use worker::{Context, Env, Request, Response};
+use argon2::{
+    password_hash::{Error, SaltString},
+    Argon2, PasswordHash, PasswordHasher, PasswordVerifier,
+};
+use rand_core::OsRng;
+use worker::{Context, Env, Method, Request, Response};
 
 #[worker::event(fetch)]
-async fn main(_req: Request, _env: Env, _ctx: Context) -> worker::Result<Response> {
+async fn main(mut req: Request, _env: Env, _ctx: Context) -> worker::Result<Response> {
     console_error_panic_hook::set_once();
-    Response::ok("Hello, worker!")
+    if req.method() != Method::Post {
+        return Response::error("only-post", 405);
+    }
+    let Ok(form) = req.form_data().await else {
+        return Response::error("invalid-form", 400);
+    };
+    let Some(f_pass) = form.get_field("pass") else {
+        return Response::error("missing-pass", 400);
+    };
+    let argon2 = Argon2::default();
+    if let Some(f_hash) = form.get_field("hash") {
+        let Ok(hash) = PasswordHash::new(f_hash.as_str()) else {
+            return Response::error("invalid-hash", 400);
+        };
+        match argon2.verify_password(f_pass.as_bytes(), &hash) {
+            Ok(_) => Response::ok("true"),
+            Err(Error::Password) => Response::ok("false"),
+            Err(_) => Response::error("verify-error", 500),
+        }
+    } else {
+        let salt = SaltString::generate(&mut OsRng);
+        let Ok(hash) = argon2.hash_password(f_pass.as_bytes(), &salt) else {
+            return Response::error("hash-error", 500);
+        };
+        Response::ok(hash.to_string())
+    }
 }
